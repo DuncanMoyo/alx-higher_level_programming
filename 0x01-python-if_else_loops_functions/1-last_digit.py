82985d856bbadb92a657d471382c25c2128a9153
DuncanMoyo <duncanfmoyo@gmail.com>
2023-06-06 10:56:15 +0000

    Pycodestyle changes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,11 @@ if number > 0:
 else:
     last_digit = number % -10
 opening = "Last digit of"
+greater = "and is greater than 5"
+less = "and is less than 6 and not 0"
 if last_digit > 5:
-    print("{} {} is {} and is greater than 5".format(opening, number, last_digit))
+    print("{} {} is {} {}".format(opening, number, last_digit, greater))
 elif last_digit == 0:
     print("{} {} is {} and is 0".format(opening, number, last_digit))
 else:
-    print("{} {} is {} and is less than 6 and not zero".format(opening, number, last_digit))
+    print("{} {} is {} {}".format(opening, number, last_digit, less))
