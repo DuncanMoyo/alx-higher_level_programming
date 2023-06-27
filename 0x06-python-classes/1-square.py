e6911a845afc658efc6df0e7518b24cc48d06dcb
DuncanMoyo <duncanfmoyo@gmail.com>
2023-06-27 11:51:42 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """class Square"""
+
+    def __init__(self, size):
+        """
+        Initiates a new square.
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
