4b34f6efd7ec9f5e883310c06e3a565706a8782e
DuncanMoyo <duncanfmoyo@gmail.com>
2023-07-02 16:42:13 +0000

    Task 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Defines a function that shows a name and surname."""
+
+
+def say_my_name(first_name, last_name=""):
+    """Print a name.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str): The last name.
+
+    Raises:
+        TypeError: If the first_name or last_name is not a string.
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
