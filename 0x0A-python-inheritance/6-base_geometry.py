0b047898bf0331c2a06920c370f2cfbc7d6611d4
DuncanMoyo <duncanfmoyo@gmail.com>
2023-07-10 18:33:09 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""class BaseGeometry (based on 5-base_geometry.py)."""
+
+
+class BaseGeometry:
+    """base class"""
+
+    def area(self):
+        """raises an exception with the appropriate message"""
+        raise Exception("area() is not implemented")
