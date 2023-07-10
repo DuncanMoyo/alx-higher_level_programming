b051706996c8b8d32d6d283dcbaa4b8b31d2f568
DuncanMoyo <duncanfmoyo@gmail.com>
2023-07-10 17:49:05 +0000

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""class MyList that inherits from list"""
+
+
+class MyList(list):
+    """inherits from MyList"""
+    def print_sorted(self):
+    """
+        inherits from the class
+    """
+    print(sorted(self))
