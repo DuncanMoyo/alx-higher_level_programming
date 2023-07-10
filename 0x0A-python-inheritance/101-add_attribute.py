9edf50a5268c625900cc8065aaaf0d31bc402906
DuncanMoyo <duncanfmoyo@gmail.com>
2023-07-10 19:25:33 +0000

    Task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Defines a function which adds an attribute to an object
+"""
+
+
+def add_attribute(obj, attr, value):
+    """Adding attribute to an object"""
+
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr, value)
