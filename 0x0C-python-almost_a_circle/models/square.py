bc8012054a225e88da9843558d55809534230c89
DuncanMoyo <duncanfmoyo@gmail.com>
2023-07-13 13:59:59 +0000

    Task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defining the square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square definition"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initiation"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns a string representation of the Square instance."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
