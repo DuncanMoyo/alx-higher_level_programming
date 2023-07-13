c32e1fa4f0e22a87147522cd5b3fa116d97633cf
DuncanMoyo <duncanfmoyo@gmail.com>
2023-07-13 13:50:25 +0000

    Task 8 - had skipped this

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,13 +98,15 @@ class Rectangle(Base):
                                                        self.width,
                                                        self.height)
 
-    def update(self, *args, **kwargs):
-        """Argument to each attribute assigned"""
-        if args:
-            attrs = ["id", "width", "height", "x", "y"]
-            for i, arg in enumerate(args):
-                setattr(self, attrs[i], arg)
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
+    def update(self, *args):
+        """Assigns an argument to each attribute"""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
