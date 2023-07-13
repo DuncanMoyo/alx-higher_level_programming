d87c9d550ece46a79804d1c076f1e52d860d3c04
DuncanMoyo <duncanfmoyo@gmail.com>
2023-07-13 13:44:31 +0000

    Task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,14 @@ class Rectangle(Base):
                                                        self.y,
                                                        self.width,
                                                        self.height)
+
+    def update(self, *args, **kwargs):
+        """Argument to each attribute assigned"""
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
