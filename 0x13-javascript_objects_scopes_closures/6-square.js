96f92963001cb6a4434e392098f59aa29be42e6a
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-12 11:20:06 +0000

    Fixed the linting

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
+const inheritedSquare = require('./5-square');
 
-const InheritedSquare = require('./5-square');
-
-class Square extends InheritedSquare {
-        constructor (size) {
-        super(size, size);
-        }
-
-	charPrint(c) {
-		if (c === undefined) {
-			c = 'X';
-		}
-	}
+class Square extends inheritedSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
 }
 
 module.exports = Square;
