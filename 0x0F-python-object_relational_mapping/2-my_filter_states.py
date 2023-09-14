4855144402dd066191b1031c8a8ef278f501c075
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-14 16:14:02 +0000

    Task 2 - Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connects to the MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+
+            port=3306)
+
+    # Executes the SQL query to retrieve all states starting with N
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
+
+    # Prints the results in the format specified in the prompt
+    for row in cur.fetchall():
+        print(row)
+
+    # Closes the cursor and database connection
+    cur.close()
+    db.close()
