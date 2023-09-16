8c640fdf0877c4e245986148908d06cf5908e0a8
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-16 08:33:43 +0000

    Task 7 - refactored

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-
+"""Start link class to table in database
 """
-Lists all State objects from the database hbtn_0e_6_usa
-"""
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    # Connects to the MySQL server running on localhost at port 3306
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
-    # Creates a session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Queries the database to retrieve all states
-    states = session.query(State).order_by(State.id).all()
-
-    # Prints the results in the format specified in the peompt
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
-    # closes the session and database connection
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
