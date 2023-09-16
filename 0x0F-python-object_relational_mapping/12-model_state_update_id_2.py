e2474bea3c919a90e47d82fe6cac9c8e4a9c0139
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-16 11:02:57 +0000

    Task 12 - refactored

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-
+""" prints the State object with the name passed as argument from the database
 """
-Changes the name of a State object from the database hbtn_0e_6_usa
-"""
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    # Connects to the MySQL server running on localhost at port 3306
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
-    # Creates a session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-     # Queries the database to retrieve the state with id = 2 and changes its name to New Mexico
-    state = session.query(State).filter(State.id == 2).first()
-    if state is not None:
-        state.name = "New Mexico"
-        session.commit()
-
-    # closes the session and database connection
-    session.close()
+    new_instance = session.query(State).filter_by(id=2).first()
+    new_instance.name = 'New Mexico'
+    session.commit()
