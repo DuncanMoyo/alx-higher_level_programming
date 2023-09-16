5cd6e0cb595ddc92b03678d48856623e4f9b9c5e
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-16 09:00:36 +0000

    Task 10 - refactored

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-
+""" prints the State object with the name passed as argument from the database
 """
-Prints the State object with the name passed as argument from the database hbtn_0e_6_usa
-"""
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    # Connects to the MySQL server running on localhost at port 3306
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
-    # Creates a session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Queries the database to retrieve the first state
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-
-    # Prints the results in the format specified in the peompt
-    if state is not None:
-        print(state.id)
-    else:
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(instance[0].id)
+    except IndexError:
         print("Not found")
-
-    # closes the session and database connection
-    session.close()
