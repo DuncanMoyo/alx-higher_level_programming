23efc8c77dba1d38900ad2c0867b6664608dc2d7
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-16 06:42:21 +0000

    Task 4 - Word count error correction

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
 """
 
 import MySQLdb
@@ -9,21 +10,15 @@ import sys
 
 if __name__ == "__main__":
     # Connects to the MySQL server running on localhost at port 3306
-    db = MySQLdb.connect(host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-
-            port=3306)
-
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     # Executes the SQL query to retrieve all states starting with N
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
-
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
     # Prints the results in the format specified in the prompt
     for row in cur.fetchall():
         print(row)
-
     # Closes the cursor and database connection
     cur.close()
     db.close()
