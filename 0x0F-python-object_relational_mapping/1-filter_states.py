97328744e7900b323272ef13aabb666f91025b11
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-16 04:02:23 +0000

    Task 1 - removed spaces

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,15 +15,12 @@ if __name__ == "__main__":
             passwd=sys.argv[2],
             db=sys.argv[3],
             port=3306)
-
     # Executes the SQL query to retrieve all states starting with N
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
     # Prints the results in the format specified in the prompt
     for row in cur.fetchall():
         print(row)
-
     # Closes the cursor and database connection
     cur.close()
     db.close()
