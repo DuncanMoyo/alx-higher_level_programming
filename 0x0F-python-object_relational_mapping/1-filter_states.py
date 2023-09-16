65de15f9d0417528578fe4b7d913d06f10f589ce
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-16 04:40:49 +0000

    Task 1 -refactored

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
-"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -13,7 +10,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id")
-    for row in cur.fetchall():
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
     cur.close()
     db.close()
