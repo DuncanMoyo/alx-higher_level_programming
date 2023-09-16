d012723b0c6262380691d58574662d0be25f85d6
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-16 04:39:17 +0000

    Task 1 - adhered to wc

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,15 +8,12 @@ import sys
 
 
 if __name__ == "__main__":
-    # Connects to the MySQL server running on localhost at port 3306
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    # Executes the SQL query to retrieve all states starting with N
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
-    # Prints the results in the format specified in the prompt
+    cur.execute("SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id")
     for row in cur.fetchall():
         print(row)
-    # Closes the cursor and database connection
     cur.close()
     db.close()
