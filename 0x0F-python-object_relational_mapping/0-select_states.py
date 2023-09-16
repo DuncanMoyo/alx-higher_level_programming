db0146c9f4e208ec7fa872ef8aac9153f3a0ad41
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-16 03:37:36 +0000

    Task 0 - switchedd to single quotes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     """
     Retrieve all rows in table 'states'
     """
-    db = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect(host='localhost',
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
             port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
 
     for row in rows:
