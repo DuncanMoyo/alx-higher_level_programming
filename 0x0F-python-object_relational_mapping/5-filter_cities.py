4d80589a80a5b5b2e1184b3e8f83253613c2bd50
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-16 07:04:07 +0000

    Task 5 - refactored again

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-
-"""
-takes in the name of a state as an argument and lists all cities of
-that state, using the database hbtn_0e_4_usa
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Connects to the MySQL server running on localhost at port 3306
-    db = MySQLdb.connect(host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306)
-    # Executes the SQL query to retrieve all cities of the given state
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-     cur.execute("""SELECT cities.name FROM
+    cur.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
                 WHERE states.name=%s""", (sys.argv[4],))
     rows = cur.fetchall()
-    # Prints the results in the format specified in the prompt
     tmp = list(row[0] for row in rows)
     print(*tmp, sep=", ")
-    # Closes the cursor and database connection
     cur.close()
     db.close()
