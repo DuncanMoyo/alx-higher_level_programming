b0564caf76df22e59800492d86df5d553ea20879
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-16 04:09:00 +0000

    Task 1 - Trying to eliminate errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 """
 Lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa
 """
-
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     # Connects to the MySQL server running on localhost at port 3306
@@ -17,7 +16,7 @@ if __name__ == "__main__":
             port=3306)
     # Executes the SQL query to retrieve all states starting with N
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     # Prints the results in the format specified in the prompt
     for row in cur.fetchall():
         print(row)
