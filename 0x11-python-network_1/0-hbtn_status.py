b7d196e79ccff19ab39a57aed27a8226880d6bbd
Duncan Moyo <duncanfmoyo@gmail.com>
2023-09-29 10:13:18 +0000

    Task 2 - POST an email #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
 
+
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
-
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
